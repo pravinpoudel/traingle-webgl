1d6180cc1c50b9e806e6950ca58078b9f74e14cf
pravinpoudel <prvnpoudel4@gmail.com>
2020-06-03 19:30:46 +0545

    vertex shader error is solved

--- js/varying.js
+++ js/varying.js
@@ -7,11 +7,20 @@ precision highp float;
 in vec2 a_position;
 in vec4 a_color;
 
+uniform vec2 u_resolution;
+
 out vec4 v_color;
 
 void main(){
-	gl_Position = vec4(a_position, 0, 1);
-	v_color = a_color;
+    
+    vec2 clipSpace = a_position/u_resolution;   
+    
+    // gl_Position = vec4(clipSpace*vec2(1, -1), 0, 1);
+
+    gl_Position = vec4(clipSpace, 0, 1);
+
+
+    v_color = a_color;
 }
 `;
 
@@ -25,7 +34,7 @@ in  vec4 v_color;
 out vec4 outColor;
 
 void main(){
-	outColor = v_color;
+    outColor = v_color;
 }
 `;
 
@@ -38,8 +47,10 @@ function main() {
     }
 
     var program = webglUtils.createProgramFromSources(gl, [vs, fs]);
+
     var vertexPosition = gl.getAttribLocation(program, 'a_position');
     var vertexColor = gl.getAttribLocation(program, 'a_color');
+    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
 
     var vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
@@ -48,12 +59,12 @@ function main() {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 
     var position = [
-        -150, -100,
-        150, -100,
-        -150, 100,
-        150, -100,
-        -150, 100,
-        150, 100
+          -150, -100,
+           150, -100,
+          -150,  100,
+           150, -100,
+          -150,  100,
+           150,  100,
     ];
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position), gl.STATIC_DRAW);
@@ -73,17 +84,21 @@ function main() {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 
     var color = [
-    Math.random(), Math.random(), Math.random(), 1,
-    Math.random(), Math.random(), Math.random(), 1,
-    Math.random(), Math.random(), Math.random(), 1,
-    Math.random(), Math.random(), Math.random(), 1,
+        Math.random(), Math.random(), Math.random(), 1,
+        Math.random(), Math.random(), Math.random(), 1,
+        Math.random(), Math.random(), Math.random(), 1,
+        Math.random(), Math.random(), Math.random(), 1,
+        Math.random(), Math.random(), Math.random(), 1,
+        Math.random(), Math.random(), Math.random(), 1
     ];
+
+    console.log(color);
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
 
     gl.enableVertexAttribArray(vertexColor);
 
-    var size =4;
+    var size = 4;
     var type = gl.FLOAT;
     var normalize = false;
     var stride = 0;
@@ -100,6 +115,8 @@ function main() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     gl.useProgram(program);
+
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
     gl.bindVertexArray(vao);
 
--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function main() {
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    gl.clearColor(0, 0, 0, 0);
+    gl.clearColor(0, 0, 0, 0);  
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.useProgram(program);
